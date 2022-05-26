991e40e3f87be1f15646855723ff275f44d0ffba
MrRobot420 <max.karlchen@web.de>
2022-05-26 10:08:22 +0200

    update app.ts with helpful additions

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,70 @@
-import express from 'express'
-import bodyParser from 'body-parser'
+import cors from 'cors'
+import helmet from 'helmet'
 import config from 'config'
+import morgan from 'morgan'
+import express, { Request, Response } from 'express'
+import rateLimit, { RateLimitRequestHandler } from 'express-rate-limit'
+import { Server } from 'http'
 import routes from './routes'
 
+class App {
+    public app: express.Application
+    private port: string
+    private logFormat: string
+    private rateLimiter: RateLimitRequestHandler = rateLimit({
+        windowMs: 60 * 1000, // 1 minute
+        max: 60,
+        message: 'You have exceeded the 60 requests in 1 minute limit!',
+        headers: true,
+    })
 
-const app = express()
-app.use(bodyParser.json({ limit: '10MB'}))
+    constructor() {
+        this.app = express()
+        this.port = process.env.PORT || config.get('PORT')
+        this.logFormat = config.get('LOG_FORMAT')
+    }
 
-app.use('/api', routes)
+    public startServer(): Server {
+        this.initializeMiddlewares()
+        this.initializeControllers()
+        this.initializeErrorHandlers()
+        return this.app.listen(this.port, () => {
+            console.group('Server details:')
+            console.log('#############################################')
+            console.log(`##  Server start time: ${Date.now()}`)
+            console.log(`##  Service name: ${process.env.npm_package_name}`)
+            console.log(`##  Server is listening on port: ${this.port}`)
+            console.log(`##  Version: ${process.env.npm_package_version}`)
+            console.log(`##  Press CTRL+C to stop the server`)
+            console.log('#############################################')
+        })
+    }
 
+    private initializeMiddlewares(): void {
+        this.app.use(cors())
+        this.app.use(helmet({ contentSecurityPolicy: false, crossOriginEmbedderPolicy: false }))
+        this.app.use(morgan(this.logFormat))
+        this.app.use(express.urlencoded({ limit: '20mb', extended: true }))
+        this.app.use(express.json({ limit: '20mb' }))
+    }
 
-const PORT = config.get('API_PORT')
+    private initializeControllers(): void {
+        this.app.use('/api', this.rateLimiter, routes)
+    }
 
-app.listen(PORT, () => {
-    console.log(`server started on port: ${PORT}`)
-})
+    private initializeErrorHandlers(): void {
+        this.app.use((req, res) => {
+            return res.status(404).send({ message: 'Route ' + req.url + ' not found.' })
+        })
+
+        this.app.use((err: Error, _req: Request, res: Response) => {
+            console.error('UNHANDLED EXCEPTION: ', err)
+            return res.status(500).json({
+                status: 'error',
+                message: err.message,
+            })
+        })
+    }
+}
+
+export default App
---
+++ src/server.ts
@@ -0,0 +1,6 @@
+import App from './app'
+
+const app = new App()
+const server = app.startServer()
+
+export default server
